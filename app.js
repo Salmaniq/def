b224875edaf04732cfe0ec710d40af2ec201ff92
Salmaniq <mohammadsalman2598@gmail.com>
2016-09-24 17:27:06 +0500

    my javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+var a=5;
+var b=5.3;
+var c="may name is xzy";
+var weight = 150;
+alert(a);
+alert(b);
+alert(c);
+var weight = 70;
+alert(70
+)
+var popularNumber = 2 + 2;
+alert(2+2)
+alert("Hellow world");
